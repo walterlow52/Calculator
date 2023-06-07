880cc49c35206d2cacff40e5a8b00dbb9c453fa0
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-06-07 19:01:26 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,45 @@
+result = False;
+for (var i = 0; i < document.querySelectorAll(".numbers").length; i++) {
+  number[i].addEventListener("click", function(e) {
+    var currentString = document.getElementById("input").innerHTML;
+    var last_ch = currentString[currentString.length-1];
+    if (result === False) {
+      document.getElementById("input").innerHTML += e.target.innerHTML;
+    }
+    else if (result === True && last_ch === "+" || last_ch === "-" || last_ch === "x" || last_ch === "/") {
+      result = False;
+      document.getElementById("input").innerHTML += e.target.innerHTML;
+    }
+    else {
+      result = False;
+      document.getElementById("input").innerHTML = "";
+      document.getElementById("input").innerHTML += e.target.innerHTML;
+    }
+  });
+}
+
+for (var i = 0; i < document.querySelectorAll(".operators").length; i++) {
+  document.querySelectorAll(".operators")[i].addEventListener("click", function(e) {
+    var currentString = document.getElementById("input").innerHTML;
+    var last_ch = currentString[currentString.length-1];
+    if (last_ch === "+" || last_ch === "-" || last_ch === "x" || last_ch === "/") {
+      var newString = currentString.substring(0, currentString.length-1) + e.target.innerHTML;
+      document.getElementById("input").innerHTML = newString;
+    }
+    else if (currentString.length == 0) {
+      console.log("Enter a number"); 
+    }
+    else {
+      document.getElementById("input").innerHTML += e.target.innerHTML;
+    }
+  });
+}
+document.getElementById("result").addEventListener("click", function() {
+  var inputString = document.getElementById("input").innerHTML;
+  var nums = inputString.split(/\+|\-|\x|\//g);
+  var operators = inputString.replace(/[0-9]|\./g, "").split("");
+  console.log(inputString);
+  console.log(operators);
+  console.log(nums);
+  console.log("------------------------------------------------------------------------------------------");
+});
